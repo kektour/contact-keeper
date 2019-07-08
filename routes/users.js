e806c4c927a11ba6a0382380c4fcc55077d27104
Alexey <elexex97@gmail.com>
2019-07-08 23:50:20 +0300

    FEATURE: Hash Passwords & Register Route

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -27,7 +28,21 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    return res.send('Passed');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+      user = new User({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      return res.send('User saved');
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send('Server error');
+    }
   }
 );
 
